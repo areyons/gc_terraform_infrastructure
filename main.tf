aea474898b9bb01cd46204c5e83b56b36b08d68a
Andrew <andreiono@ukr.net>
2021-04-04 19:15:08 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 resource "google_compute_network" "nat-example" {
     project = "some-project-id"
     name = "nat-example"
+    auto_create_subnetworks = false
     mtu = 1460
     routing_mode = "REGIONAL"
 }
